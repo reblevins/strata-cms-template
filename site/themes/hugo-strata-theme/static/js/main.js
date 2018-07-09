fca6988bac04c997db75063ee70e2111231d04e8
Rodney Blevins <rodney@blevins.nl>
2018-07-08 21:31:48 -0600

    header, about

--- site/themes/hugo-strata-theme/static/js/main.js
+++ site/themes/hugo-strata-theme/static/js/main.js
@@ -84,9 +84,9 @@
 
 							$header.css('background-position', 'left 0px');
 
-							$window.on('scroll.strata_parallax', function() {
-								$header.css('background-position', 'left ' + (-1 * (parseInt($window.scrollTop()) / settings.parallaxFactor)) + 'px');
-							});
+							// $window.on('scroll.strata_parallax', function() {
+							// 	$header.css('background-position', 'left ' + (-1 * (parseInt($window.scrollTop()) / settings.parallaxFactor)) + 'px');
+							// });
 
 						}
 
